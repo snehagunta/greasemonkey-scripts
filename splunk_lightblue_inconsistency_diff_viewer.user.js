d343c106fdd3055698c3def26e44f08ae4e559e7
Yusuf Koer <ykoer@redhat.com>
2015-11-13 09:46:08 +0100

    Restructure splunk_lightblue_inconsistency_diff_viewer.user.js, add poosibility to change the diff modes

--- splunk_lightblue_inconsistency_diff_viewer.user.js
+++ splunk_lightblue_inconsistency_diff_viewer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        splunk_lightblue_inconsistency_diff_viewer
 // @description Splunk lightblue inconsistency diff viewer. It is using jsdifflib library (https://github.com/cemerick/jsdifflib).
-// @version     0.3
+// @version     0.4
 // @namespace   _splunk_lightblue
 // @include     https://splunk.corp.redhat.com/*
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
@@ -19,9 +19,19 @@
 // @downloadURL https://raw.githubusercontent.com/ykoer/greasemonkey-scripts/master/splunk_lightblue_inconsistency_diff_viewer.user.js
 // ==/UserScript==
 
-//--- Add our custom dialog using jQuery. Note the multi-line string syntax.
+//--- Add the diff dialog window using jQuery. Note the multi-line string syntax.
 $("body").append (
-    '<div id="gmOverlayDialog"></div>'
+    '<div id="gmOverlayDialog"> \
+        <div class="top"> \
+            <ul><li style="list-style:none;"> \
+                Context size (optional):<input type="text" id="contextSize" value="" style="width: 50px;"/>&nbsp; \
+                <input type="radio" name="_viewtype" id="sidebyside"/ value="0" checked>Side by Side Diff&nbsp; \
+                <input type="radio" name="_viewtype" id="inline" value="1"/>inline&nbsp; \
+                <button class="json-splunk splIconicButton splButton-tertiary" id="applyViewTypeChange">Apply</button> \
+            </li></ul> \
+        </div> \
+        <div id="diffoutput"></div> \
+    </div>'
 );
 
 var iconSet1        = GM_getResourceURL ("IconSet1");
@@ -34,44 +44,139 @@ jqUI_CssSrc         = jqUI_CssSrc.replace (/images\/ui-icons_454545_256x240\.png
 
 GM_addStyle (jqUI_CssSrc);
 GM_addStyle (diffView_CssSrc);
+GM_addStyle (".li {display:inline}");
 
 
 function main() {
 
+    // holds the json of the selected log message for diffing
+    var legacyJson, lightblueJson;
+
+    // lookup map. It's currenly not used since window[map[methodName]] is not working with GM
     var map = {
         'getEngineeringProductsForSkus': 'sortEngineeringProduct2Dim',
         'getEngineeringProductsByOID': 'sortEngineeringProduct1Dim',
         'getProducts': 'sortOperationalProduct1Dim',
-    } 
+    }
+
+    // register events
+    $("#applyViewTypeChange").click(function () {
+        createDiff();
+    });
+
+    function createDiff() {
+        var contextSize = $("#contextSize").val() || null;
+        var type = parseInt($('input[name=_viewtype]:checked').val());
+        var legacy = difflib.stringAsLines(JSON.stringify(legacyJson, null, 3));
+        var lightblue = difflib.stringAsLines(JSON.stringify(lightblueJson, null, 3));
+        var sm = new difflib.SequenceMatcher(legacy, lightblue);
+        var opcodes = sm.get_opcodes();
+        var diffoutputdiv = $("#diffoutput");
+
+        diffoutputdiv.empty();
+        diffoutputdiv.append(diffview.buildView({
+            baseTextLines: legacy,
+            newTextLines: lightblue,
+            opcodes: opcodes,
+            baseTextName: "Legacy response",
+            newTextName: "Lightblue response",
+            contextSize: contextSize,
+            viewType: type
+        }));
+    }
+
+    function showDiffWidget() {
+        createDiff();
+        $("#gmOverlayDialog").dialog("open");
+    }
 
     function parseLog(log) {
+        console.log("1");
+
         i1 = log.indexOf('Inconsistency found in ')+23;
         i2 = log.indexOf('(',i1);
         i3 = log.indexOf(':',i2);
         i4 = log.indexOf('legacyJson: ',i3);
         i5 = log.indexOf(', lightblueJson: ',i4);
 
-        var api = log.substring(i1,i3);
-        var methodName = log.substring(i1,i2);
-        var legacyJson = log.substring(i4+12,i5);
-        var lightblueJson = log.substring(i5+17);
+        if(i4>0 && i5>0) {
 
-        json1 = sortJson(legacyJson, methodName);
-        json2 = sortJson(lightblueJson, methodName);
+            var api = log.substring(i1,i3);
+            var methodName = log.substring(i1,i2);
+            var legacyJsonLog = log.substring(i4+12,i5);
+            var lightblueJsonLog = log.substring(i5+17);
 
-        showDiffWidget(json1, json2);
-    }
+            legacyJson = sortJson(legacyJsonLog, methodName);
+            lightblueJson = sortJson(lightblueJsonLog, methodName);
 
-    function sortJson(jsonString, method) {
-        var json = JSON.parse(jsonString);
-
-        switch (method) {
-            case 'getEngineeringProductsForSkus': return sortEngineeringProduct2Dim(json);
-            case 'getEngineeringProductsByOID': sortEngineeringProduct1Dim(json);
-            case 'getProducts': return sortOperationalProduct1Dim(json);
+            showDiffWidget();
+        } else {
+            alert("Can not show the diff since the json payload is too long!");
         }
-        return json;
     }
+
+    function draw_buttons () {
+        var items = $('tr.shared-eventsviewer-list-body-row');
+        items.each(function (index, el_tmp) {
+
+            if ($(el_tmp).find('td._time button').length === 0) {
+                var log = $(el_tmp).find('td._time').parentsUntil('tbody').last().find('div.raw-event').text();
+
+                if(log.indexOf('Inconsistency found in')>0) {
+                    var el = $(el_tmp).find('td._time'),
+                        container,
+                        format_button = $('<button>');
+                    format_button.text('Inconsistency Diff');
+                    format_button.addClass('json-splunk splIconicButton splButton-tertiary');
+                    format_button.on('click', function() {
+                        parseLog(log);
+                    });
+                    el.append(format_button);
+                }
+
+            }
+        });
+    }
+
+    function initDiffWidget() {
+        $("#gmOverlayDialog").dialog ( {
+            modal:      true,
+            autoOpen:   false,
+            title:      "Inconsistency Diff",
+            width:      1200,
+            height:     700,
+            zIndex:     83666,   //-- This number doesn't need to get any higher.
+            open: function () {
+                $(this).scrollTop(0);
+            }
+        });
+    }
+
+    // Try to draw the buttons every 1000ms.  Previously, this was done by
+    // creating a Splunk.Module.EventsViewer.prototype.onResultsRendered
+    // function, but Splunk.Module is no longer defined in Splunk 6.1.4,
+    // even though docs imply that it should be.  Not sure why the previous
+    // approach no longer works, but this hacky interval will do until I
+    // can figure out how to wire up the event again.
+    setInterval(function() {
+        initDiffWidget();
+        draw_buttons();
+    }.bind(this), 1000);
+
+    /* -------------------------- sort functions -------------------------------------*/
+
+    function predicateBy(prop){
+        return function(a,b){
+            if( a[prop] > b[prop]){
+                return 1;
+            } else if( a[prop] < b[prop] ){
+                return -1;
+            }
+            return 0;
+        }
+    }
+
+    // TODO: can be optimized since there is a lot of shared logic
 
     function sortEngineeringProduct2Dim(json) {
         for(i=0; i<json.length; i++) {
@@ -88,6 +193,15 @@ function main() {
                         }
                     }
                 }
+            }
+        }
+        return json;
+    }
+
+    function sortOperationalProduct1Dim(json) {
+        for(i=0; i<json.length; i++) {
+            if (json[i].attributes != null) {
+                json[i].attributes.sort( predicateBy("code") );
             }
         }
         return json;
@@ -113,99 +227,15 @@ function main() {
         return json;
     }
 
-    function sortOperationalProduct1Dim(json) {
-        for(i=0; i<json.length; i++) {
-            if (json[i].attributes != null) {
-                json[i].attributes.sort( predicateBy("code") );
-            }
+    function sortJson(jsonString, method) {
+        var json = JSON.parse(jsonString);
+        switch (method) {
+            case 'getEngineeringProductsForSkus': return sortEngineeringProduct2Dim(json);
+            case 'getEngineeringProductsByOID': sortEngineeringProduct1Dim(json);
+            case 'getProducts': return sortOperationalProduct1Dim(json);
         }
         return json;
     }
-
-    function predicateBy(prop){
-        return function(a,b){
-            if( a[prop] > b[prop]){
-                return 1;
-            } else if( a[prop] < b[prop] ){
-                return -1;
-            }
-            return 0;
-        }
-    }
-    
-    function draw_buttons () {
-        var items = $('tr.shared-eventsviewer-list-body-row');
-        items.each(function (index, el_tmp) {
-
-            if ($(el_tmp).find('td._time button').length === 0) {
-                var log = $(el_tmp).find('td._time').parentsUntil('tbody').last().find('div.raw-event').text();
-
-                if(log.indexOf('Inconsistency found in')>0) {
-                    var el = $(el_tmp).find('td._time'),
-                        container,
-                        format_button = $('<button>');
-                    format_button.text('Inconsistency Diff');
-                    format_button.addClass('json-splunk splIconicButton splButton-tertiary');
-                    format_button.on('click', function() {
-                        parseLog(log);
-                    });
-                    el.append(format_button);
-                }
-
-            }
-        });
-    }
-    
-    function showDiffWidget(json1, json2) {
-        
-        createDiff(json1, json2);
-        var w = $(window).width();
-        var h = $(window).height();
-        
-        $("#gmOverlayDialog").dialog ( {
-            modal:      true,
-            title:      "Inconsistency Diff",
-            width:      1200,
-            height:     700,
-            zIndex:     83666   //-- This number doesn't need to get any higher.
-        });
-        
-        $("html, body").animate({ scrollTop: 0 }, "fast");
-    }
-    
-    function createDiff(json1, json2) {
-
-        var legacy = difflib.stringAsLines(JSON.stringify(json1, null, 3));
-        var lightblue = difflib.stringAsLines(JSON.stringify(json2, null, 3));
-        var sm = new difflib.SequenceMatcher(legacy, lightblue);
-        var opcodes = sm.get_opcodes();
-        var diffoutputdiv = $("#gmOverlayDialog");
-        var contextSize = "";
-
-
-        diffoutputdiv.innerHTML = "";
-        contextSize = contextSize || null;
-
-        diffoutputdiv.append(diffview.buildView({
-            baseTextLines: legacy,
-            newTextLines: lightblue,
-            opcodes: opcodes,
-            baseTextName: "Legacy response",
-            newTextName: "Lightblue response",
-            contextSize: contextSize,
-            viewType: 0
-        }));
-    }
-
-    // Try to draw the buttons every 1000ms.  Previously, this was done by
-    // creating a Splunk.Module.EventsViewer.prototype.onResultsRendered
-    // function, but Splunk.Module is no longer defined in Splunk 6.1.4,
-    // even though docs imply that it should be.  Not sure why the previous
-    // approach no longer works, but this hacky interval will do until I
-    // can figure out how to wire up the event again.
-    setInterval(function() {
-        draw_buttons();
-    }.bind(this), 1000);
 }
 
 main();
